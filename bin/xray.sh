67797a735e6d08b6e64a195ce8b1ad87b46489a3
sunliang711 <sunliang711@163.com>
2025-08-07 11:40:33 +0800

    new xray.sh

--- bin/xray.sh
+++ bin/xray.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
+
 if [ -z "${BASH_SOURCE}" ]; then
     this=${PWD}
-    logfile="/tmp/$(%FT%T).log"
 else
     rpath="$(readlink ${BASH_SOURCE})"
     if [ -z "$rpath" ]; then
         rpath=${BASH_SOURCE}
+    elif echo "$rpath" | grep -q '^/'; then
+        # absolute path
+        echo
+    else
+        # relative path
+        rpath="$(dirname ${BASH_SOURCE})/$rpath"
     fi
     this="$(cd $(dirname $rpath) && pwd)"
-    logfile="/tmp/$(basename ${BASH_SOURCE}).log"
 fi
-
-source ${this}/../config.sh || { echo "Source config.sh failed"; exit 1; }
-export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 user="${SUDO_USER:-$(whoami)}"
 home="$(eval echo ~$user)"
 
-# export TERM=xterm-256color
-
-# Use colors, but only if connected to a terminal, and that terminal
-# supports them.
+# 定义颜色
+# Use colors, but only if connected to a terminal(-t 1), and that terminal supports them(ncolors >=8.
 if which tput >/dev/null 2>&1; then
-  ncolors=$(tput colors 2>/dev/null)
+    ncolors=$(tput colors 2>/dev/null)
 fi
 if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
     RED="$(tput setaf 1)"
     GREEN="$(tput setaf 2)"
     YELLOW="$(tput setaf 3)"
     BLUE="$(tput setaf 4)"
-    CYAN="$(tput setaf 5)"
+    # 品红色
+    MAGENTA=$(tput setaf 5)
+    # 青色
+    CYAN="$(tput setaf 6)"
+    # 粗体
     BOLD="$(tput bold)"
     NORMAL="$(tput sgr0)"
 else
@@ -42,114 +46,139 @@ else
     NORMAL=""
 fi
 
-_err(){
-    echo "$*" >&2
+# 日志级别常量
+LOG_LEVEL_FATAL=1
+LOG_LEVEL_ERROR=2
+LOG_LEVEL_WARNING=3
+LOG_LEVEL_SUCCESS=4
+LOG_LEVEL_INFO=5
+LOG_LEVEL_DEBUG=6
+
+# 默认日志级别
+LOG_LEVEL=$LOG_LEVEL_INFO
+
+# 导出 PATH 环境变量
+export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+err_require_command=100
+err_require_root=200
+err_require_linux=300
+err_create_dir=400
+
+_command_exists() {
+    command -v "$1" >/dev/null 2>&1
 }
 
-_command_exists(){
-    command -v "$@" > /dev/null 2>&1
+_require_command() {
+    if ! _command_exists "$1"; then
+        echo "Require command $1" 1>&2
+        exit ${err_require_command}
+    fi
+}
+
+_require_commands() {
+    errorNo=0
+    for i in "$@";do
+        if ! _command_exists "$i"; then
+            echo "need command $i" 1>&2
+            errorNo=$((errorNo+1))
+        fi
+    done
+
+    if ((errorNo > 0 ));then
+        exit ${err_require_command}
+    fi
+}
+
+function _ensureDir() {
+    local dirs=$@
+    for dir in ${dirs}; do
+        if [ ! -d ${dir} ]; then
+            mkdir -p ${dir} || {
+                echo "create $dir failed!"
+                exit $err_create_dir
+            }
+        fi
+    done
 }
 
 rootID=0
 
-_runAsRoot() {
-    local trace=0
-    local subshell=0
-    local nostdout=0
-    local nostderr=0
+function _root() {
+    if [ ${EUID} -ne ${rootID} ]; then
+        echo "need root privilege." 1>&2
+        return $err_require_root
+    fi
+}
 
-    local optNum=0
-    for opt in ${@}; do
-        case "${opt}" in
-        --trace | -x)
-            trace=1
-            ((optNum++))
+function _require_root() {
+    if ! _root; then
+        exit $err_require_root
+    fi
+}
+
+function _linux() {
+    if [ "$(uname)" != "Linux" ]; then
+        echo "need Linux" 1>&2
+        return $err_require_linux
+    fi
+}
+
+function _require_linux() {
+    if ! _linux; then
+        exit $err_require_linux
+    fi
+}
+
+function _wait() {
+    # secs=$((5 * 60))
+    secs=${1:?'missing seconds'}
+
+    while [ $secs -gt 0 ]; do
+        echo -ne "$secs\033[0K\r"
+        sleep 1
+        : $((secs--))
+    done
+    echo -ne "\033[0K\r"
+}
+
+function _parseOptions() {
+    if [ $(uname) != "Linux" ]; then
+        echo "getopt only on Linux"
+        exit 1
+    fi
+
+    options=$(getopt -o dv --long debug --long name: -- "$@")
+    [ $? -eq 0 ] || {
+        echo "Incorrect option provided"
+        exit 1
+    }
+    eval set -- "$options"
+    while true; do
+        case "$1" in
+        -v)
+            VERBOSE=1
             ;;
-        --subshell | -s)
-            subshell=1
-            ((optNum++))
+        -d)
+            DEBUG=1
             ;;
-        --no-stdout)
-            nostdout=1
-            ((optNum++))
+        --debug)
+            DEBUG=1
             ;;
-        --no-stderr)
-            nostderr=1
-            ((optNum++))
+        --name)
+            shift # The arg is next in position args
+            NAME=$1
             ;;
-        *)
+        --)
+            shift
             break
             ;;
         esac
+        shift
     done
-
-    shift $(($optNum))
-    local cmd="${*}"
-    bash_c='bash -c'
-    if [ "${EUID}" -ne "${rootID}" ]; then
-        if _command_exists sudo; then
-            bash_c='sudo -E bash -c'
-        elif _command_exists su; then
-            bash_c='su -c'
-        else
-            cat >&2 <<-'EOF'
-			Error: this installer needs the ability to run commands as root.
-			We are unable to find either "sudo" or "su" available to make this happen.
-			EOF
-            return 1
-        fi
-    fi
-
-    local fullcommand="${bash_c} ${cmd}"
-    if [ $nostdout -eq 1 ]; then
-        cmd="${cmd} >/dev/null"
-    fi
-    if [ $nostderr -eq 1 ]; then
-        cmd="${cmd} 2>/dev/null"
-    fi
-
-    if [ $subshell -eq 1 ]; then
-        if [ $trace -eq 1 ]; then
-            (
-                { set -x; } 2>/dev/null
-                ${bash_c} "${cmd}"
-            )
-        else
-            (${bash_c} "${cmd}")
-        fi
-    else
-        if [ $trace -eq 1 ]; then
-            { set -x; } 2>/dev/null
-            ${bash_c} "${cmd}"
-            local ret=$?
-            { set +x; } 2>/dev/null
-            return $ret
-        else
-            ${bash_c} "${cmd}"
-        fi
-    fi
 }
 
-function _insert_path(){
-    if [ -z "$1" ];then
-        return
-    fi
-    echo -e ${PATH//:/"\n"} | grep -c "^$1$" >/dev/null 2>&1 || export PATH=$1:$PATH
-}
-
-_run(){
-    # only output stderr
-    cmd="${*}"
-    (set -x; bash -c "${cmd}" >> ${logfile})
-}
-
-function _root(){
-    if [ ${EUID} -ne ${rootID} ];then
-        echo "Requires root privilege."
-        exit 1
-    fi
-}
-
+# 设置ed
 ed=vi
 if _command_exists vim; then
     ed=vim
@@ -158,13 +187,151 @@ if _command_exists nvim; then
     ed=nvim
 fi
 # use ENV: editor to override
-if [ -n "${editor}" ];then
+if [ -n "${editor}" ]; then
     ed=${editor}
 fi
-###############################################################################
-# write your code below (just define function[s])
-# function is hidden when begin with '_'
-###############################################################################
+
+rootID=0
+_checkRoot() {
+    if [ "$(id -u)" -ne 0 ]; then
+        # 检查是否有 sudo 命令
+        if ! command -v sudo >/dev/null 2>&1; then
+            echo "Error: 'sudo' command is required." >&2
+            return 1
+        fi
+
+        # 检查用户是否在 sudoers 中
+        echo "Checking if you have sudo privileges..."
+        if ! sudo -v 2>/dev/null; then
+            echo "You do NOT have sudo privileges or failed to enter password." >&2
+            return 1
+        fi
+    fi
+}
+
+_runAsRoot() {
+    if [ "$(id -u)" -eq 0 ]; then
+        echo "Running as root: $*"
+        "$@"
+    else
+        if ! command -v sudo >/dev/null 2>&1; then
+            echo "Error: 'sudo' is required but not found." >&2
+            return 1
+        fi
+        echo "Using sudo: $*"
+        sudo "$@"
+    fi
+}
+
+# 日志级别名称数组及最大长度计算
+LOG_LEVELS=("FATAL" "ERROR" "WARNING" "INFO" "SUCCESS" "DEBUG")
+MAX_LEVEL_LENGTH=0
+
+for level in "${LOG_LEVELS[@]}"; do
+  len=${#level}
+  if (( len > MAX_LEVEL_LENGTH )); then
+    MAX_LEVEL_LENGTH=$len
+  fi
+done
+MAX_LEVEL_LENGTH=$((MAX_LEVEL_LENGTH+2))
+
+# 日志级别名称填充
+pad_level() {
+  printf "%-${MAX_LEVEL_LENGTH}s" "[$1]"
+}
+
+# 打印带颜色的日志函数
+log() {
+  local level="$(echo "$1" | awk '{print toupper($0)}')" # 转换为大写以支持大小写敏感
+  shift
+  local message="$@"
+  local padded_level=$(pad_level "$level")
+  local timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+  case "$level" in
+    "FATAL")
+      if [ $LOG_LEVEL -ge $LOG_LEVEL_FATAL ]; then
+        echo -e "${RED}${BOLD}[$timestamp] $padded_level${NC} $message${NORMAL}"
+        exit 1
+      fi
+      ;;
+
+    "ERROR")
+      if [ $LOG_LEVEL -ge $LOG_LEVEL_ERROR ]; then
+        echo -e "${RED}${BOLD}[$timestamp] $padded_level${NC} $message${NORMAL}"
+      fi
+      ;;
+    "WARNING")
+      if [ $LOG_LEVEL -ge $LOG_LEVEL_WARNING ]; then
+        echo -e "${YELLOW}${BOLD}[$timestamp] $padded_level${NC} $message${NORMAL}"
+      fi
+      ;;
+    "INFO")
+      if [ $LOG_LEVEL -ge $LOG_LEVEL_INFO ]; then
+        echo -e "${BLUE}${BOLD}[$timestamp] $padded_level${NC} $message${NORMAL}"
+      fi
+      ;;
+    "SUCCESS")
+      if [ $LOG_LEVEL -ge $LOG_LEVEL_SUCCESS ]; then
+        echo -e "${GREEN}${BOLD}[$timestamp] $padded_level${NC} $message${NORMAL}"
+      fi
+      ;;
+    "DEBUG")
+      if [ $LOG_LEVEL -ge $LOG_LEVEL_DEBUG ]; then
+        echo -e "${CYAN}${BOLD}[$timestamp] $padded_level${NC} $message${NORMAL}"
+      fi
+      ;;
+    *)
+      echo -e "${NC}[$timestamp] [$level] $message${NORMAL}"
+      ;;
+  esac
+}
+
+# 设置日志级别的函数
+set_log_level() {
+  local level="$(echo "$1" | awk '{print toupper($0)}')"
+  case "$level" in
+    "FATAL")
+      LOG_LEVEL=$LOG_LEVEL_FATAL
+      ;;
+    "ERROR")
+      LOG_LEVEL=$LOG_LEVEL_ERROR
+      ;;
+    "WARNING")
+      LOG_LEVEL=$LOG_LEVEL_WARNING
+      ;;
+    "INFO")
+      LOG_LEVEL=$LOG_LEVEL_INFO
+      ;;
+    "SUCCESS")
+      LOG_LEVEL=$LOG_LEVEL_SUCCESS
+      ;;
+    "DEBUG")
+      LOG_LEVEL=$LOG_LEVEL_DEBUG
+      ;;
+    *)
+      echo "无效的日志级别: $1"
+      ;;
+  esac
+}
+
+# 显示帮助信息
+show_help() {
+  echo "Usage: $0 [-l LOG_LEVEL] <command>"
+  echo ""
+  echo "Commands:"
+  for cmd in "${COMMANDS[@]}"; do
+    echo "  $cmd"
+  done
+  echo ""
+  echo "Options:"
+  echo "  -l LOG_LEVEL  Set the log level (FATAL ERROR, WARNING, INFO, SUCCESS, DEBUG)"
+}
+
+# ------------------------------------------------------------
+# 子命令数组
+COMMANDS=("help" "add" "list" "config" "start" "stop" "restart" "log" "traffic" "remove" )
+source ${this}/../config.sh || { echo "Source config.sh failed"; exit 1; }
+
 beginCron="#begin v2relay cron"
 endCron="#end v2relay cron"
 clashGroup=clash
@@ -192,7 +359,7 @@ _genServiceFile(){
     local name=${1:?'missing config name'}
     # genfrontend ${name}.json
     echo "+ Generate ${name}.json from ${name}.yaml"
-    _run "(${appsDir}/genfrontend/genfrontend -t ${appsDir}/genfrontend/frontendTemplate -c ${etcDir}/${name}.yaml -o ${etcDir}/${name}.json)" || { echo "${RED}failed!"; exit 1; }
+    (${appsDir}/genfrontend/genfrontend -t ${appsDir}/genfrontend/frontendTemplate -c ${etcDir}/${name}.yaml -o ${etcDir}/${name}.json) || { echo "${RED}failed!"; exit 1; }
 
     local start_pre="${binDir}/xray.sh _start_pre ${name}"
     local start="${appsDir}/xray/xray run -c ${etcDir}/${name}.json"
@@ -225,10 +392,10 @@ _genServiceFile(){
         -e "s|<PWD>|${pwd}|g" \
         ${templateDir}/xray.service > /tmp/xray-${name}.service
 
-    _runAsRoot "mv /tmp/xray-${name}.service /etc/systemd/system"
-    _runAsRoot "systemctl daemon-reload"
-    _runAsRoot "sudo systemctl enable xray-${name}.service"
-    # _runAsRoot "sudo systemctl restart xray-${name}.service"
+    _runAsRoot mv /tmp/xray-${name}.service /etc/systemd/system
+    _runAsRoot systemctl daemon-reload
+    _runAsRoot sudo systemctl enable xray-${name}.service
+    # _runAsRoot sudo systemctl restart xray-${name}.service
 }
 
 list(){
@@ -257,13 +424,13 @@ start(){
     local configName=${1:?'missing config file name (just name,no yaml extension)'}
     configName="${configName%.yaml}"
     _genServiceFile $configName
-    _runAsRoot "systemctl start xray-${configName}.service"
+    _runAsRoot systemctl start xray-${configName}.service
 }
 
 stop(){
     local configName=${1:?'missing config file name (just name,no yaml extension)'}
     configName="${configName%.yaml}"
-    _runAsRoot "systemctl stop xray-${configName}.service"
+    _runAsRoot systemctl stop xray-${configName}.service
 }
 
 restart(){
@@ -276,7 +443,7 @@ restart(){
 log(){
     local configName=${1:?'missing config file name (just name,no yaml extension)'}
     configName="${configName%.yaml}"
-    _runAsRoot "journalctl -u xray-${configName} -f"
+    _runAsRoot journalctl -u xray-${configName} -f
     # sudo journalctl -u xray-${configName} -f
 
 }
@@ -360,18 +527,18 @@ remove(){
         exit 1
     fi
     echo "Remove ${configName}..."
-    _runAsRoot "systemctl stop xray-${configName}.service"
-    _runAsRoot "/bin/rm -rf /etc/snystemd/system/xray-${configName}.service"
-    _run "/bin/rm -rf ${etcDir}/${configName}.yaml"
-    _run "/bin/rm -rf ${etcDir}/${configName}.json"
+    _runAsRoot systemctl stop xray-${configName}.service
+    _runAsRoot /bin/rm -rf /etc/systemd/system/xray-${configName}.service
+    /bin/rm -rf ${etcDir}/${configName}.yaml
+    /bin/rm -rf ${etcDir}/${configName}.json
 }
 
 _removeAll(){
     cd ${etcDir}
     for etc in $(ls *.yaml 2>/dev/null);do
         remove ${etc%.yaml}
-        _run "/bin/rm -rf ${etc%.yaml}.json"
-        _run "/bin/rm -rf ${etc}"
+        /bin/rm -rf ${etc%.yaml}.json
+        /bin/rm -rf ${etc}
     done
 }
 
@@ -379,23 +546,43 @@ em(){
     $ed $0
 }
 
-###############################################################################
-# write your code above
-###############################################################################
-function _help(){
-    cd "${this}"
-    cat<<EOF2
-Usage: $(basename $0) ${bold}CMD${reset}
 
-${bold}CMD${reset}:
-EOF2
-    perl -lne 'print "\t$2" if /^\s*(function)?\s*(\S+)\s*\(\)\s*\{$/' $(basename ${BASH_SOURCE}) | perl -lne "print if /^\t[^_]/"
-}
+# ------------------------------------------------------------
 
-case "$1" in
-     ""|-h|--help|help)
-        _help
-        ;;
-    *)
-        "$@"
+# 解析命令行参数
+while getopts ":l:" opt; do
+  case ${opt} in
+    l )
+      set_log_level "$OPTARG"
+      ;;
+    \? )
+      show_help
+      exit 1
+      ;;
+    : )
+      echo "Invalid option: $OPTARG requires an argument" 1>&2
+      show_help
+      exit 1
+      ;;
+  esac
+done
+# NOTE: 这里全局使用了OPTIND，如果在某个函数中也使用了getopts，那么在函数的开头需要重置OPTIND (OPTIND=1)
+shift $((OPTIND -1))
+
+# 解析子命令
+command=$1
+shift
+
+if [[ -z "$command" ]]; then
+  show_help
+  exit 0
+fi
+
+case "$command" in
+  help)
+    show_help
+    ;;
+  *)
+    ${command} "$@"
+    ;;
 esac
